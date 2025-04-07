d171e1b01f3a97b94d1703c39c0f1c391d305c08
Jade Cong <jade.cong@qq.com>
2025-04-07 10:17:32 +0800

    update copyright

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -132,7 +132,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} OrderForce Intelligence, Inc.`,
     },
     prism: {
       theme: prismThemes.github,
